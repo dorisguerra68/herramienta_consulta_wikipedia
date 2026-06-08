20889d33e780faf6ab3f7cf386dd5e7060dadfc9
--replace-all <loretodoris68@gmail.com>
2026-06-08 16:02:50 +0200

    instalación de las diferentes librerias necesarias

--- content_enricher/translator/TranslatorClient.py
+++ content_enricher/translator/TranslatorClient.py
@@ -1,17 +1,13 @@
 from deep_translator import GoogleTranslator
 
 class TranslatorClient:
+
     def __init__(self):
         pass
-    def translate_text(self,text:str, source_language:str, target_language:str):
-            translated_text = GoogleTranslator(
-                source=source_language,
-                target=target_language
-            ).translate(text)
-            return translated_text
 
-translator = TranslatorClient()
-
-print(translator.translate_text(text='Hola familia', source_language='auto', target_language='it'))
-
-
+    def translate_text(self, text: str, source_language: str, target_language: str):
+        translated_text = GoogleTranslator(
+            source=source_language,
+            target=target_language
+        ).translate(text)
+        return translated_text
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# This is a sample Python script.
+
+from translator.TranslatorClient import TranslatorClient
+from app.App import get_user_input
+
+#UI
+text = get_user_input("What would you like to translate?")
+src_language = get_user_input("From What language?")
+tgt_language = get_user_input("To What language?")
+
+#logic
+#Nos crean una instancia de una clase =OBJETO de clase.
+translator = TranslatorClient().translate_text(
+    text=text,
+    source_language=src_language,
+    target_language=tgt_language
+)
+
+
+print(translator)
